339d217c67889203bade0bcf9f96b208f1293050
Satyam Vishwakarma <satyamvishwakarma01.dev@gmail.com>
2025-08-06 20:45:30 +0530

    Add login functionality and menu to Main
    Introduced a login method in Commands to validate admin credentials. Updated Main to prompt for username and password, display a menu upon successful login, and handle user choices for library actions.

--- Commands.java
+++ Commands.java
@@ -1,3 +1,7 @@
-public class Commands {
+import java.util.Objects;
 
+public class Commands {
+    public boolean login(String userName, String password) {
+        return Objects.equals(userName, "admin") && Objects.equals(password, "1234");
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Commands cmd = new Commands();
+        String user, pass;      //string var
+        int choice;             //int var
 
-    }
-}
+        Scanner sc = new Scanner(System.in);    //User Input
+
+        //while
+        while (true) {
+            //login
+            System.out.print("User Name: ");
+            user = sc.nextLine();
+            System.out.print("Password: ");
+            pass = sc.nextLine();
+            if (cmd.login(user, pass)) {
+                System.out.println("Welcome, Admin");
+                System.out.println("""
+                        Choose from the following:
+                        1 -> Add Book to library
+                        2 -> Check Book
+                        3 -> Issue Book
+                        4 -> Return Book
+                        5 -> Exit""");
+                //choices
+                System.out.print("-> ");
+                choice = sc.nextInt();
+
+                //choice 1
+                if (choice==1){
+                    break;
+                }//end choice 1
+
+                //choice to end
+                else if (choice==5){
+                    System.out.println("Thank You for using our service\n" +
+                            "Have a Great Day!!");
+                    return;
+                }
+            } else {
+                System.out.println("INVALID USERNAME/PASSWORD");
+            }//end login if-else
+        }//end while
+    }//end main
+}//end class
